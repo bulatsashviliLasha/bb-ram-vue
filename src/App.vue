96fddde9b9a4d6f87c0c0a86058585a9d3e1233f
Lasha Bulatsashvili <lashareichert@gmail.com>
2022-11-28 14:47:47 +0400

    feat: add project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import BreakingBadCards from "@/components/BreakingBadCards.vue"
+</script>
+
+<template>
+  <main>
+    <Suspense>
+      <template #default>
+        <BreakingBadCards/>
+      </template>
+      <template #fallback>
+        <div>
+          <div>Loading...</div>
+        </div>
+      </template>
+    </Suspense>
+  </main>
+</template>
+
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { NCard } from "naive-ui";
+
+const props = defineProps(["image", "name", "occupation"]);
+</script>
+
+<template>
+  <NCard>
+    <template #cover>
+      <img :src="props.image" />
+    </template>
+    <h3>{{ props.name }}</h3>
+    <div class="jobs">
+      <p v-for="(job, index) in props.occupation" :key="job + index">
+        {{ job }}<span v-if="index < occupation.length - 1">, </span>
+      </p>
+    </div>
+  </NCard>
+</template>
+
+<style lang="scss" scoped>
+.n-card {
+  width: 200px;
+  margin: 10px 20px;
+  img {
+    height: 250px;
+    object-fit: cover;
+  }
+}
+
+p {
+  font-size: 10px;
+}
+
+.jobs {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
